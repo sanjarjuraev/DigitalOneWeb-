8407fc1b3e77367254f37462b45a282130696618
Abdulloh Juraev <sanjar.j@nightwell-logistics.com>
2024-10-29 11:56:09 +0500

    fixed

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,8 +3,10 @@ import cRm from '/src/assets/bg-images/web-crm.png';
 import tMs from '/src/assets/bg-images/web-tms.png';
 import eRp from '/src/assets/bg-images/web-erp.png';
 import { motion } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
   return (
     <section className="relative text-gray-800 pt-28 sm:pt-20 xs:pt-40 md:pt-20 lg:pt-40  lg:pb-40 sm:pb-20">
       <div className="container mx-auto px-6   flex flex-col md:flex-row items-center  gap-6">
@@ -30,19 +32,35 @@ const Hero = () => {
           {/* First and Second Columns */}
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 sm:grid-cols-1 items-end">
             <motion.div
-              initial={{
-                rotate: -90,
-                x: 0,
-                y: 0,
-                opacity: 0,
-              }}
-              whileInView={{
-                rotate: 0,
-                y: 0,
-                x: 0,
-                opacity: 1,
-                transition: { duration: 1 },
-              }}
+              initial={
+                isMobile
+                  ? {
+                      opacity: 0,
+                      // Slide from right if index is odd, else from left
+                      x: 50,
+                    }
+                  : {
+                      rotate: -90,
+                      x: 0,
+                      y: 0,
+                      opacity: 0,
+                    }
+              }
+              whileInView={
+                isMobile
+                  ? {
+                      opacity: 1,
+                      x: 0,
+                      transition: { duration: 1 },
+                    }
+                  : {
+                      rotate: 0,
+                      y: 0,
+                      x: 0,
+                      opacity: 1,
+                      transition: { duration: 1 },
+                    }
+              }
               viewport={{ once: true }}
               className="relative flex items-center justify-center  bg-gray-100 md:w-[200px] md:h-[175px] w-[300px] h-[300px] overflow-hidden rounded-lg shadow-md mx-auto border-double border-4 border-red-600"
             >
@@ -56,19 +74,35 @@ const Hero = () => {
               </span>
             </motion.div>
             <motion.div
-              initial={{
-                rotate: 90,
-                x: 0,
-                y: 0,
-                opacity: 0,
-              }}
-              whileInView={{
-                rotate: 0,
-                y: 0,
-                x: 0,
-                opacity: 1,
-                transition: { duration: 1, delay: 0.8 },
-              }}
+              initial={
+                isMobile
+                  ? {
+                      opacity: 0,
+                      // Slide from right if index is odd, else from left
+                      x: -50,
+                    }
+                  : {
+                      rotate: 90,
+                      x: 0,
+                      y: 0,
+                      opacity: 0,
+                    }
+              }
+              whileInView={
+                isMobile
+                  ? {
+                      opacity: 1,
+                      x: 0,
+                      transition: { duration: 1 },
+                    }
+                  : {
+                      rotate: 0,
+                      y: 0,
+                      x: 0,
+                      opacity: 1,
+                      transition: { duration: 1, delay: 0.8 },
+                    }
+              }
               viewport={{ once: true }}
               className="relative flex items-center justify-center  bg-gray-100 md:w-[200px] md:h-[200px] w-[300px] h-[300px]  rounded-lg shadow-md mx-auto overflow-hidden border-double border-4 border-red-600"
             >
